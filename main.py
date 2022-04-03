6cd135547c8507c215b52369d53304d32912532d
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-04-03 10:59:30 +0200

    Removed sandwicher folder, may be added back alte4r

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #Imports
-from Sandwicher.squisher import *
-import PySimpleGUI as sg
+from squisher import *
 
-#Body
-sg.Window(title="Password", layout=[[]], margins=(500, 500)).read()
 
+getPass()
---
+++ squisher.py
@@ -0,0 +1,30 @@
+# Imports
+from Numbers.num2 import *
+from Numbers.num3 import *
+from Numbers.num4 import *
+from Numbers.num5 import *
+from Numbers.num6 import *
+from Numbers.num7 import *
+from Characters.char2 import *
+from Characters.char3 import *
+from Characters.char4 import *
+from Characters.char5 import *
+from Characters.char6 import *
+
+
+# Function
+def getPass():
+    # Main
+    
+    password1 = findFirst1()
+    password2 = findFirst2()
+    password3 = findFirst3()
+    password4 = findFirst4()
+    password5 = findFirst5()
+    password6 = findFirst6()
+    char1 = findChar1()
+    char2 = findChar2()
+    char3 = findChar3()
+    char4 = findChar4()
+    char5 = findChar5()
+    print(password1, char1, password2, char2, password3, char3, password4, char4, password5, char5, password1, char1, password2, char2, password3, char3, password4, char4, password5, char5 )
