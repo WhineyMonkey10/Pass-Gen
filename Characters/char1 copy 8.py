1f47ef457ca9071ddeb349d50c61e33731ac2973
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-04-03 10:36:56 +0200

    Started work on special characteres

---
+++ Characters/char1 copy 8.py
@@ -0,0 +1,6 @@
+import random
+def findChar1():
+    chars1 = ["&", "%", ")", "{", "^°", "´´", "?", "+", "~", "#"]
+    numb1 = (random.randint(1, 9))
+    randomnpasschar = (chars1[numb1])
+    return randomnpasschar
